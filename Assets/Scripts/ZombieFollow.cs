86caabfcbd94db6c817592cd822d5f65388420b8
mat <mateo.gibert@epitech.eu>
2025-07-27 01:19:52 +0200

    added the zombie collisition

--- Assets/Scripts/ZombieFollow.cs
+++ Assets/Scripts/ZombieFollow.cs
@@ -1,23 +1,59 @@
+using System;
 using UnityEngine;
 
 public class ZombieFollow : MonoBehaviour
 {
+    
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+    private const string isMoving = "IsMoving";
+    private GameObject target;
+    private Animator animator;
+    
+    bool isFollowing = false;
+    
+    
     [SerializeField, Range(1f, 100f)]
     private float speed = 1f;
     Transform player;
+    
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.gameObject.tag == "Player")
+      {
+          target = other.gameObject;
+          isFollowing = true;
+      }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFollowing = false;
+            animator.SetBool(isMoving, false);
+        }
+    }
     private void Update()
     {
-        if(player == null) return;
-        Vector3 direction = player.position - transform.position;
-        Vector3 velocity = direction.normalized * speed;
-        transform.position  += velocity*Time.deltaTime;
-        
-        //faire un rayon ou le joueur est detecter 
-        //choper les positions du zombie et ajouter un rayon de base pour detecter le joueur 
+            if (!isFollowing || player == null) return;
+            Vector3 direction = player.position - transform.position;
+            Vector3 velocity = direction.normalized * speed;
+            transform.position += velocity * Time.deltaTime;
+
+            animator.SetFloat(horizontal, velocity.y < 0.5 && velocity.y > -0.5 ? velocity.x : 0);
+            animator.SetFloat(vertical, velocity.x < 0.5 && velocity.x > -0.5 ? velocity.y : 0);
+            animator.SetBool(isMoving, velocity.sqrMagnitude >= 0.01f);
         
     }
 }
