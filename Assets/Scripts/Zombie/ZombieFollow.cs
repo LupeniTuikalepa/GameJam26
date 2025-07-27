dfcf04fc02f5a4b75958fa460c888260e0e9ef97
tom-marchandise <tom.marchandise@epitech.eu>
2025-07-27 14:20:08 +0200

    Added the attacks from the player, the health of th zombies and the attack animations

--- Assets/Scripts/Zombie/ZombieFollow.cs
+++ Assets/Scripts/Zombie/ZombieFollow.cs
@@ -1,37 +1,39 @@
 using System;
 using UnityEngine;
 
-public class ZombieFollow : MonoBehaviour
+public partial class ZombieFollow : MonoBehaviour
 {
-    
+
     private const string horizontal = "Horizontal";
     private const string vertical = "Vertical";
     private const string isMoving = "IsMoving";
     private Animator animator;
-    
+
     bool isFollowing = false;
-    
-    
+
     [SerializeField, Range(1f, 100f)]
     private float speed = 1f;
     Transform player;
-    
+    private Rigidbody2D rb;
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        health = maxHealth;
     }
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-      if (other.gameObject.tag == "Player")
-      {
-          isFollowing = true;
-      }
+        if (other.gameObject.tag == "Player")
+        {
+            isFollowing = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -42,16 +44,21 @@ public class ZombieFollow : MonoBehaviour
             animator.SetBool(isMoving, false);
         }
     }
+
+
     private void Update()
     {
-            if (!isFollowing || player == null) return;
-            Vector3 direction = player.position - transform.position;
-            Vector3 velocity = direction.normalized * speed;
-            transform.position += velocity * Time.deltaTime;
+        animator.SetFloat(horizontal, Mathf.Abs(rb.linearVelocityY) < 0.5 ? rb.linearVelocityX : 0);
+        animator.SetFloat(vertical, Mathf.Abs(rb.linearVelocityX) < 0.5 ? rb.linearVelocityY : 0);
+        animator.SetBool(isMoving, rb.linearVelocity.sqrMagnitude >= 0.01f);
 
-            animator.SetFloat(horizontal, velocity.y < 0.5 && velocity.y > -0.5 ? velocity.x : 0);
-            animator.SetFloat(vertical, velocity.x < 0.5 && velocity.x > -0.5 ? velocity.y : 0);
-            animator.SetBool(isMoving, velocity.sqrMagnitude >= 0.01f);
-        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isFollowing || player == null) return;
+        Vector3 direction = player.position - transform.position;
+        Vector3 velocity = direction.normalized * speed;
+        rb.linearVelocity = velocity;
     }
 }
