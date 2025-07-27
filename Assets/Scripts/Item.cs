18aa705a8475260f39f2f566d9b0d193051be6a5
tom-marchandise <tom.marchandise@epitech.eu>
2025-07-27 09:48:56 +0200

    Added the animation when picking up an object

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
-public class Object : MonoBehaviour
+public class Item : MonoBehaviour, IInteractable
 {
+    public bool IsTaken { get; private set; }
+    public string ItemId { get; private set; }
+    public GameObject itemPrefab;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ItemId ??= GlobalHelper.GenerateUniqueId(itemPrefab);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public bool CanInteract()
     {
-        
+        return !IsTaken;
+    }
+
+    public void Interact()
+    {
+        if (!CanInteract()) return;
+        TakeItem();
+    }
+
+    private void TakeItem()
+    {
+        // Set IsTaken
+        setTaken(true);
+        // Destroy Item
+        if (itemPrefab)
+        {
+            Destroy(itemPrefab);
+        }
+    }
+
+    public void setTaken(bool taken)
+    {
+        IsTaken = taken;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using LTX.ChanneledProperties.Priorities;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,25 +12,44 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rb;
     private Animator _animator;
 
+    private PlayerMovement playerMovement;
+
     private const string _horizontal = "Horizontal";
     private const string _vertical = "Vertical";
     private const string _isMoving = "IsMoving";
+
+    private const string _isInteracting = "IsInteracting";
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
 
         _rb.linearVelocity = _movement * _moveSpeed;
 
-        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_horizontal, _movement.y == 0 ? _movement.x : 0);
         _animator.SetFloat(_vertical, _movement.y);
         _animator.SetBool(_isMoving, _movement.sqrMagnitude >= 0.01f);
+    }
+
+    public void TriggerInteractionAnimation()
+    {
+            _animator.SetBool(_isInteracting, true);
+    }
+    
+    public void StopInteractionAnimation()
+    {
+        _animator.SetBool(_isInteracting, false);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _movement = context.ReadValue<Vector2>();
     }
 }
